b62e21adf17e8779dc62cc2cfc49fb8d49cdb610
Ghostboo124 <perkinsal@student.jpc.qld.edu.au>
2025-01-31 13:33:06 +1000

    Adjusting a few things

--- high-low.py
+++ high-low.py
@@ -1,10 +1,11 @@
 from random import randint
 
 guess = ""
+guessed = False
 number = randint(0,100)
 counter = 1
 
-while guess != number:
+while not guessed:
     try:
         guess = int(input("Make a guess between 0 and 100! "))
     except ValueError:
@@ -19,6 +20,9 @@ while guess != number:
         print("Too high")
     elif guess <= number:
         print("Too low")
+    elif guess == number:
+        print("Correct")
+        guessed = True
     else:
         print("Uhhh something has gone severly wrong")
         exit(1)
