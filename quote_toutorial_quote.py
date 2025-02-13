a3f215704428e5b47cabe3991ee97108bdddca8a
Ghostboo124 <perkinsal@student.jpc.qld.edu.au>
2025-02-14 09:15:02 +1000

    Adding (flawed) collision detection

--- quote_toutorial_quote.py
+++ quote_toutorial_quote.py
@@ -4,34 +4,76 @@ This is a game where you shoot an apple
 #import pgzero
 from random import randint
 import pygame
-import quote_toutorial_quote
+import quote_toutorial_quote as variables
 import time
 
 font = pygame.font.SysFont(None, 24)
 apple = Actor("apple")
+pineapple = Actor("pineapple")
+orange = Actor("orange")
 winCounter = 0
 loseCounter = 0
+start = True
 
 def draw() -> None:
+    if variables.start:
+        for i in range(0,4):
+            place_apple()
+            place_pineapple()
+            place_orange()
+        variables.start = False
     screen.clear()
     apple.draw()
+    pineapple.draw()
+    orange.draw()
     t0 = time.time()
-    counter1 = quote_toutorial_quote.font.render(f'Win Counter: {quote_toutorial_quote.winCounter}', True, pygame.Color(255,255,255))
-    counter2 = quote_toutorial_quote.font.render(f'Lose Counter: {quote_toutorial_quote.loseCounter}', True, pygame.Color(255,255,255))
+    counter1 = variables.font.render(f'Win Counter: {variables.winCounter}', True, pygame.Color(255,255,255))
+    counter2 = variables.font.render(f'Lose Counter: {variables.loseCounter}', True, pygame.Color(255,255,255))
     print('time needed for Font creation :', time.time()-t0)
     screen.blit(counter1, (10, 10))
     screen.blit(counter2, (10, 30))
 
 def place_apple() -> None:
-    #apple.x, apple.y = (768, 565)
     apple.x, apple.y = (randint(35, 768), randint(40, 565))
+    if orange.collidepoint((apple.x, apple.y)):
+        print("Apple has collided with the orange")
+        place_apple()
+    if pineapple.collidepoint((apple.x, apple.y)):
+        print("Apple has collided with the pineapple")
+        place_apple()
+
+def place_pineapple() -> None:
+    pineapple.x, pineapple.y = (randint(50,750), randint(80,515))
+    if orange.collidepoint((pineapple.x, pineapple.y)):
+        print("Pineapple has collided with the orange")
+        place_pineapple()
+    if apple.collidepoint((pineapple.x, pineapple.y)):
+        print("Pineapple has collided with the apple")
+        place_pineapple()
+
+def place_orange() -> None:
+    orange.x, orange.y = (randint(50, 750), randint(59,535))
+    if pineapple.collidepoint((orange.x, orange.y)):
+        print("Orange has collided with the pineapple")
+        place_orange()
+    if apple.collidepoint((orange.x, orange.y)):
+        print("Orange has collided with the apple")
+        place_orange()
 
 def on_mouse_down(pos) -> None:
     if apple.collidepoint(pos):
         print('Good Shot!')
         place_apple()
-        quote_toutorial_quote.winCounter += 1
+        variables.winCounter += 1
+    elif pineapple.collidepoint(pos):
+        print('Good Shot!')
+        place_pineapple()
+        variables.winCounter += 1
+    elif orange.collidepoint(pos):
+        print('Good Shot!')
+        place_orange()
+        variables.winCounter += 1
     else:
         print("You missed!")
-        quote_toutorial_quote.winCounter = 0
-        quote_toutorial_quote.loseCounter += 1
+        variables.winCounter = 0    
+        variables.loseCounter += 1
